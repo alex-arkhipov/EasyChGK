ec1659f7cbfb201df76265495e3e5271404532ca
Alex Arkhipov <simeon.arkhipov@gmail.com>
2018-01-05 00:26:59 +0300

    * Main.storyboard: Changed numeric keyboard for number of question   settings
    * SettingsViewController.cs: Added settings saving in preferences even
      when game is unloaded from memory

--- SettingsViewController.cs
+++ SettingsViewController.cs
@@ -6,23 +6,33 @@ namespace EasyChGK
 {
     public partial class SettingsViewController : UITableViewController
     {
-        public SettingsViewController (IntPtr handle) : base (handle)
+        const string KEY_QUESTION_QUANTITY = "qq";
+        const string KEY_SHOW_TIPS = "tips";
+
+        protected SettingsViewController(IntPtr handle) : base(handle)
         {
+            // Note: this .ctor should not contain any initialization logic.
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            LoadPreferences();
+            SaveSettings();
+
             var game = Neo.ChgkGame.GetGame();
+
             int n = game.GetNumOfQuestions();
             questionQuantityText.Text = n.ToString();
+
             ShowTipsSwitch.On = game.GetShowTips();
 
             SaveButton.TouchUpInside += (object sender, EventArgs e) => {
                 Console.WriteLine("SettingsViewController: SaveButton pushed");
-                // TODO: Implement Save functionality
+
                 SaveSettings();
+                SavePreferences();
                 this.NavigationController.PopViewController(true);
             };
         }
@@ -31,28 +41,74 @@ namespace EasyChGK
         {
             base.ViewDidDisappear(animated);
             SaveSettings();
+            SavePreferences();
         }
 
-        private void SaveSettings()
+        // Save preferences in application preferences
+        private void SavePreferences()
         {
-            var game = Neo.ChgkGame.GetGame();
+            using(var ns = NSUserDefaults.StandardUserDefaults)
+            {
+                // Question Quantity
+                int n = GetQuestionQuantity();
+                if (n!= 0)
+                {
+                    ns.SetInt(n, KEY_QUESTION_QUANTITY);
+                }
 
-            // Quation quantity
+                // Show tips
+                ns.SetBool(ShowTipsSwitch.On, KEY_SHOW_TIPS);
+            }
+        }
+
+        private void LoadPreferences()
+        {
+            using (var ns = NSUserDefaults.StandardUserDefaults)
+            {
+                // Question quantity
+                int n = (int)(ns.IntForKey(KEY_QUESTION_QUANTITY));
+                if (n != 0)
+                {
+                    questionQuantityText.Text = n.ToString();
+                }
+
+                // Show tips
+                ShowTipsSwitch.On = ns.BoolForKey(KEY_SHOW_TIPS);
+            }
+        }
+
+        private int GetQuestionQuantity()
+        {
             String t = questionQuantityText.Text;
             int n = 0;
             try
             {
                 n = Int32.Parse(t);
-                game.SetNumOfQuestions(n);
-            } catch (System.FormatException)
+                if (n < 1) { n = 0; }
+            }
+            catch (System.FormatException)
             {
                 Console.WriteLine("Cannot convert to int (" + t + "). No changes.");
-                return;
+            }
+            return n;
+        }
+
+        // Save settings in Game object
+        private void SaveSettings()
+        {
+            var game = Neo.ChgkGame.GetGame();
+
+            // Quation quantity
+            int n = GetQuestionQuantity();
+            if (n != 0)
+            {
+                game.SetNumOfQuestions(n);
             }
 
             // Show tips
             bool s = ShowTipsSwitch.On;
             game.SetShowTips(s);
+
         }
 
         public override void ViewWillAppear(bool animated)
